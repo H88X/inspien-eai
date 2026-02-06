0cf4120f5f1267f5771643acc29e5143fd4c1fe4
seo <kopouser24@gmail.com>
2026-02-06 22:37:28 +0900

    Main 커밋

--- src/main/java/com/inspien/eai/Main.java
+++ src/main/java/com/inspien/eai/Main.java
@@ -1,7 +1,27 @@
 package com.inspien.eai;
 
+import com.inspien.eai.controller.OrderController;
+import com.inspien.eai.scheduler.ShipmentScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            // REST API 서버 시작
+            OrderController.startServer();
+
+            // 배치 스케줄러 시작
+            new ShipmentScheduler().start();
+
+            logger.info("Application Started");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
